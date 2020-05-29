38bfde27269ce81037552c00ec5b3e5a281d366d
Nidheesh Jain <nidheeshjain01@gmail.com>
2020-05-29 12:09:34 +0530

    Added Email validation, styled blogcontent and title

--- js/blogpage_base.js
+++ js/blogpage_base.js
@@ -40,9 +40,19 @@ function moveDrone() {
 	}, 10);
 }
 
+function isEmail(email) {
+	var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	return regex.test(email);
+}
+
 $(document).ready(function(){
     $('#subscribebutton').on('click', function(){
-        $('#subscribeform').submit();
+		if (isEmail($('input[name ="Email"]')[0].value)){
+			$('#subscribeform').submit();
+		}
+		else {
+			alert('Please enter a valid Email address')
+		}
     });
 });
 
